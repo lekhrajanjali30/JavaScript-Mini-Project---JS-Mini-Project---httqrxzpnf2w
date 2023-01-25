8ad4afb9f89d5578f4ce7f84c1eaa301b99270b4
lekhrajanjali30 <lekhrajanjali@gmail.com>
2023-01-25 14:05:40 +0530

    updated

--- App.js
+++ App.js
@@ -1,1 +1,41 @@
 
+let email = document.getElementById('Email');
+let password = document.getElementById('Password');
+
+function validated() {
+	// e.preventDefault();
+	if(emailIdVerify() && passwordVerify()){
+		return true;
+	}
+	else{
+		return false;
+	}
+
+}
+
+let emailIdVerify = () => {
+	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+		email.style.border = "1px solid silver";
+		email_error.style.display = "none";
+		return true;
+	} else {
+		email.style.border = "1px solid red";
+		email_error.style.display = "block";
+		email.focus();
+		return false;
+	}
+};
+
+let passwordVerify = ()=> {
+	if(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/.test(password.value)){
+		password.style.border = "1px solid silver";
+		pass_error.style.display = "none";
+		return true;
+	}
+	else {
+		password.style.border = "1px solid red";
+		pass_error.style.display = "block";
+		password.focus();
+		return false;
+	}
+}
